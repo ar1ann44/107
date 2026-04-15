151e2a64ae2b199dd8fcc234ae6a4ea2855b33a6
Ariana Osuna <osuna.jenifer@uabc.edu.mx>
2026-04-14 22:22:26 -0700

    product changes

--- my-react-store/src/components/Product.jsx
+++ my-react-store/src/components/Product.jsx
@@ -3,7 +3,7 @@ import QuantityPicker from "./QuantityPicker";
 function Product() {
     return (
         <div className="card" style={{ width: "18rem" }}>
-            <img src="https://plus.unsplash.com/premium_photo-1724249990837-f6dfcb7f3eaa?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..." />
+            <img src="https://images.unsplash.com/photo-1619546813926-a78fa6372cd2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="text-start">Apple</h5>
                 <h6 className="text-start">$ 3.99</h6>
--- my-react-store/src/pages/Catalog.jsx
+++ my-react-store/src/pages/Catalog.jsx
@@ -11,6 +11,7 @@ function Catalog() {
                 <Product />
                 <Product />
 
+
             </div>
 
         </div>
